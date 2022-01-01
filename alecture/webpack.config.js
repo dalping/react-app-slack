4c8e1147532f9d8a476f9b72797d301535e1c0bc
Romini's coding <74459925+dalping@users.noreply.github.com>
2022-01-01 22:53:33 +0900

    .

---
+++ alecture/webpack.config.js
@@ -0,0 +1,59 @@
+const prod = process.env.NODE_ENV === "production";
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: prod ? "production" : "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: __dirname + "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".ts", ".tsx", ".js", ".json"],
+        },
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  devtool: prod ? undefined : "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+
+  devServer: {
+    historyApiFallback: true, // react router
+    port: 3000,
+    publicPath: "/dist/",
+    proxy: {
+      "/api/": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+};
+
+if (isDevelopment && config.plugins) {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(new ReactRefreshWebpackPlugin());
+  config.plugins.push(
+    new BundleAnalyzerPlugin({ analyzerMode: "server", openAnalyzer: true })
+  );
+}
+if (!isDevelopment && config.plugins) {
+  config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: "static" }));
+}
