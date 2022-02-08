5a7ec915bd2a20c34c75ac3bef0df96933674edc
Romini's coding <74459925+dalping@users.noreply.github.com>
2022-02-08 23:38:14 +0900

    [feat] useSocket

--- alecture/src/hooks/useSocket.tsx
+++ alecture/src/hooks/useSocket.tsx
@@ -1,27 +1,33 @@
 import io from "socket.io-client";
 import React, { useCallback } from "react";
 
-const sockets = {};
+//ts에서 빈배열이나 빈객체에는 타이핑을 해줘야 한다!!!!!!
+const sockets: { [key: string]: SocketIOClient.Socket } = {};
 const backUrl = `http://localhost:3095`;
+
 const useSocket = (workspace: string) => {
-  if (!workspace) return;
+  const disconnect = useCallback(() => {
+    sockets[workspace].disconnect;
+    delete sockets[workspace];
+  }, [workspace]);
+
+  if (!workspace) return [undefined, disconnect];
   sockets[workspace] = io.connect(`${backUrl}/ws-${workspace}`);
   //   const socket = io.connect(`${backUrl}`);
   //보내기
-  sockets[workspace].emit("hello", "world");
-  //받기
-  sockets[workspace].on("message", (data: any) => {
-    console.log(data);
-  });
-  sockets[workspace].on("data", (data: any) => {
-    console.log(data);
-  });
-  sockets[workspace].on("onlineList", (data: any) => {
-    console.log(data);
-  });
-  const disconnect = sockets[workspace].disconnect;
+  //   sockets[workspace].emit("hello", "world");
+  //   //받기
+  //   sockets[workspace].on("message", (data: any) => {
+  //     console.log(data);
+  //   });
+  //   sockets[workspace].on("data", (data: any) => {
+  //     console.log(data);
+  //   });
+  //   sockets[workspace].on("onlineList", (data: any) => {
+  //     console.log(data);
+  //   });
 
-  return;
+  return [sockets[workspace], disconnect];
 };
 
 export default useSocket;
